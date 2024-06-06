442b7408dd18469e4bf14e85c62fc1dfca9e3bf8
thealamdev <shahalam141072@gmail.com>
2024-06-06 20:01:34 +0600

    Data has been get from the form has been completed

--- app/Livewire/VehicleManagement/Stack/Modules/Model/CreateComponent.php
+++ app/Livewire/VehicleManagement/Stack/Modules/Model/CreateComponent.php
@@ -4,9 +4,38 @@ namespace App\Livewire\VehicleManagement\Stack\Modules\Model;
 
 use Livewire\Component;
 use Livewire\Attributes\Title;
+use App\Models\VehicleManagement\Modules\Models;
 
 class CreateComponent extends Component
 {
+    /**
+     * @var <string>
+     * public property $name
+     */
+    public $name = '';
+
+    /**
+     * @var <boolean>
+     * public property $status
+     */
+    public $status;
+
+    /**
+     * method for save data
+     * @return array|object
+     */
+    public function save(): array|object
+    {
+        dd($this->name);
+        $response = Models::create([
+            'user_id' => auth()->user()->id,
+            'name' => $this->name,
+            'status' => $this->status,
+        ]);
+
+        return back()->with('success', 'Data has been submited');
+    }
+
     #[Title('Model Create')]
     public function render()
     {
