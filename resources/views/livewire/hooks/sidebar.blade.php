771fb8091b271145d470b43947c55ffdd23fdf84
thealamdev <shahalam141072@gmail.com>
2024-06-06 17:06:32 +0600

    Accounts official sidebar and route redesign has been working

--- resources/views/livewire/hooks/sidebar.blade.php
+++ resources/views/livewire/hooks/sidebar.blade.php
@@ -232,29 +232,37 @@
                      <div class="nav-header">
                          <span>Vehicle Management</span>
                      </div>
+                     <div class="menu-group">
+                         <ul class="navbar-nav flex-column">
+                             <li class="nav-item {{ Route::is('admin.version1.vehicle-management.vehicle.index') ? 'active' : '' }}">
+                                 <a class="nav-link"
+                                     href="{{ route('admin.version1.vehicle-management.vehicle.index') }}">
+                                     <span class="nav-icon-wrap">
+                                         <span class="svg-icon">
+                                             <svg xmlns="http://www.w3.org/2000/svg"
+                                                 class="icon icon-tabler icon-tabler-template" width="24"
+                                                 height="24" viewBox="0 0 24 24" stroke-width="2"
+                                                 stroke="currentColor" fill="none" stroke-linecap="round"
+                                                 stroke-linejoin="round">
+                                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                 <rect x="4" y="4" width="16" height="4" rx="1" />
+                                                 <rect x="4" y="12" width="6" height="8" rx="1" />
+                                                 <line x1="14" y1="12" x2="20"
+                                                     y2="12" />
+                                                 <line x1="14" y1="16" x2="20"
+                                                     y2="16" />
+                                                 <line x1="14" y1="20" x2="20"
+                                                     y2="20" />
+                                             </svg>
+                                         </span>
+                                     </span>
+                                     <span class="nav-link-text">Vehicle</span>
+                                 </a>
+                             </li>
+                         </ul>
+                     </div>
                      <ul class="navbar-nav flex-column">
                          <li class="nav-item">
-                             <a class="nav-link" href="{{ route('admin.version1.vehicle-management.vehicle.index') }}">
-                                 <span class="nav-icon-wrap">
-                                     <span class="svg-icon">
-                                         <svg xmlns="http://www.w3.org/2000/svg"
-                                             class="icon icon-tabler icon-tabler-calendar-time" width="24"
-                                             height="24" viewBox="0 0 24 24" stroke-width="2"
-                                             stroke="currentColor" fill="none" stroke-linecap="round"
-                                             stroke-linejoin="round">
-                                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                             <path
-                                                 d="M11.795 21h-6.795a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v4" />
-                                             <circle cx="18" cy="18" r="4" />
-                                             <path d="M15 3v4" />
-                                             <path d="M7 3v4" />
-                                             <path d="M3 11h16" />
-                                             <path d="M18 16.496v1.504l1 1" />
-                                         </svg>
-                                     </span>
-                                 </span>
-                                 <span class="nav-link-text">Vehicle</span>
-                             </a>
                              <a class="nav-link" href="javascript:void(0);" data-bs-toggle="collapse"
                                  data-bs-target="#dash_pages">
                                  <span class="nav-icon-wrap">
@@ -273,7 +281,7 @@
                                  </span>
                                  <span class="nav-link-text">Modules</span>
                              </a>
-                             <ul id="dash_pages" class="nav flex-column collapse  nav-children">
+                             <ul id="dash_pages" class="nav flex-column collapse nav-children">
                                  <li class="nav-item">
                                      <ul class="nav flex-column">
                                          <li class="nav-item">
@@ -282,7 +290,7 @@
                                                  data-bs-toggle="collapse" data-bs-target="#dash_log">
                                                  <span class="nav-link-text">Create</span>
                                              </a>
-                                             <ul id="dash_log" class="nav flex-column collapse  nav-children">
+                                             {{-- <ul id="dash_log" class="nav flex-column collapse  nav-children">
                                                  <li class="nav-item">
                                                      <ul class="nav flex-column">
                                                          <li class="nav-item">
@@ -299,14 +307,14 @@
                                                          </li>
                                                      </ul>
                                                  </li>
-                                             </ul>
+                                             </ul> --}}
                                          </li>
                                          <li class="nav-item">
                                              <a class="nav-link" href="javascript:void(0);" data-bs-toggle="collapse"
                                                  data-bs-target="#dash_sign">
                                                  <span class="nav-link-text">Sign Up</span>
                                              </a>
-                                             <ul id="dash_sign" class="nav flex-column collapse  nav-children">
+                                             <ul id="dash_sign" class="nav flex-column collapse nav-children">
                                                  <li class="nav-item">
                                                      <ul class="nav flex-column">
                                                          <li class="nav-item">
