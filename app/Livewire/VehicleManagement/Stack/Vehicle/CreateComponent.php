ff8844d7494cd0f001a48da8c44801a319b0a016
thealamdev <shahalam141072@gmail.com>
2024-06-06 21:00:16 +0600

    Vehicle create page title set

--- app/Livewire/VehicleManagement/Stack/Vehicle/CreateComponent.php
+++ app/Livewire/VehicleManagement/Stack/Vehicle/CreateComponent.php
@@ -3,9 +3,11 @@
 namespace App\Livewire\VehicleManagement\Stack\Vehicle;
 
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
 class CreateComponent extends Component
 {
+    #[Title('Vehicle Create')]
     public function render()
     {
         return view('livewire.vehicle-management.stack.vehicle.create-component');
