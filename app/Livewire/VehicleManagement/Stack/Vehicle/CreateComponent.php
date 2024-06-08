7d035059bc84672e63350eb6a648541509033163
thealamdev <shahalam141072@gmail.com>
2024-06-08 11:05:20 +0600

    Working on vehicle create page

--- app/Livewire/VehicleManagement/Stack/Vehicle/CreateComponent.php
+++ app/Livewire/VehicleManagement/Stack/Vehicle/CreateComponent.php
@@ -4,6 +4,9 @@ namespace App\Livewire\VehicleManagement\Stack\Vehicle;
 
 use Livewire\Component;
 use Livewire\Attributes\Title;
+use App\Models\VehicleManagement\Modules\Color;
+use App\Models\VehicleManagement\Modules\Models;
+use App\Models\VehicleManagement\Modules\ModelYear;
 
 class CreateComponent extends Component
 {
@@ -11,7 +14,13 @@ class CreateComponent extends Component
      * public property $name
      * @var string
      */
-    public $name;
+    public $form = [];
+
+    /**
+     * public bind array
+     * @var array
+     */
+    public $bindData;
 
     /**
      * public property $status
@@ -19,7 +28,35 @@ class CreateComponent extends Component
      */
     public $status;
 
+    /**
+     * Define models property
+     * @var array
+     */
+    public $models = [];
 
+    /**
+     * Define modelYears property
+     * @var array
+     */
+    public $modelYears = [];
+
+    /**
+     * Define colors property
+     * @var array
+     */
+    public $colors = [];
+
+
+    /**
+     * Define the mount 
+     * @return void
+     */
+    public function mount(): void
+    {
+        $this->models = Models::query()->latest()->where('status', 1)->get();
+        $this->modelYears = ModelYear::query()->latest()->where('status', 1)->get();
+        $this->colors = Color::query()->latest()->where('status', 1)->get();
+    }
 
     #[Title('Vehicle Create')]
     public function render()
--- resources/views/livewire/vehicle-management/stack/vehicle/create-component.blade.php
+++ resources/views/livewire/vehicle-management/stack/vehicle/create-component.blade.php
@@ -3,14 +3,14 @@
         <!-- Header Part Start !-->
         <header class="pb-4 d-flex justify-content-between align-items-center">
             <div class="">
-                <p class="fs-5 fw-600">Model Year Create</p>
+                <p class="fs-5 fw-600">Vehicle Create</p>
                 <span>Please must fill the field where (*) sign is visible.</span>
             </div>
 
             <div>
-                <button class="bg-transparent border border-slate-400 px-4 py-1 rounded" type="button">
-                    Model Year
-                </button>
+                <a href="#" class="bg-transparent border border-slate-400 px-4 py-1 rounded" type="button">
+                   Vehicle
+                </a>
             </div>
         </header>
         <hr>
@@ -22,27 +22,85 @@
                 @csrf
                 <div class="row">
                     <div class="col-lg-6">
-                        <label for="name">Vehicle Name<span class="text-danger">*</span></label>
+                        <label for="form.name">Vehicle Name<span class="text-danger">*</span></label>
                         <div class="input-group mb-3">
                             <span class="input-group-text"><i class="fa-solid fa-car"></i></span>
-                            <input type="text" wire:model.live="form.name"
-                                class="form-control @error('form.name')
-                            {{ 'is-invalid' }}
-                        @enderror"
-                                placeholder="vehicle name">
+                            <input type="text" wire:model="form.name" class="form-control @error('form.name') {{ 'is-invalid' }} @enderror" placeholder="vehicle name">
                             @error('form.name')
                                 <div class="invalid-feedback">{{ $message }}</div>
                             @enderror
                         </div>
                     </div>
                     <div class="col-lg-6">
-                        <label for="status">Status<span class="text-danger">*</span></label>
+                        <label for="form.model">Model<span class="text-danger">*</span></label>
                         <div class="input-group mb-3">
                             <span class="input-group-text"><i class="fa-solid fa-star"></i></span>
-                            <select wire:model.live="form.status"
-                                class="form-control @error('form.status')
-                            {{ 'is-invalid' }}
-                        @enderror">
+                            <select wire:model="form.model" class="form-control @error('form.model') {{ 'is-invalid' }} @enderror">
+                                <option selected value>-- Please Select Status --</option>
+                                @foreach ($models as $each)
+                                  <option value="{{$each->id}}">{{ $each->name }}</option>
+                                @endforeach
+                            </select>
+                            @error('form.model')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <label for="form.modelYear">Model Year<span class="text-danger">*</span></label>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text"><i class="fa-solid fa-star"></i></span>
+                            <select wire:model="form.modelYear" class="form-control @error('form.modelYear') {{ 'is-invalid' }} @enderror">
+                                <option selected value>-- Please Select Status --</option>
+                                @foreach ($modelYears as $each)
+                                  <option value="{{$each->id}}">{{ $each->name }}</option>
+                                @endforeach
+                            </select>
+                            @error('form.modelYear')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <label for="form.color">Color<span class="text-danger">*</span></label>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text"><i class="fa-solid fa-star"></i></span>
+                            <select wire:model="form.color" class="form-control @error('form.color') {{ 'is-invalid' }} @enderror">
+                                <option selected value>-- Please Select Status --</option>
+                                @foreach ($colors as $each)
+                                  <option value="{{$each->id}}">{{ $each->name }}</option>
+                                @endforeach
+                            </select>
+                            @error('form.color')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <label for="form.registratrion_number">Registration Number<span class="text-danger">*</span></label>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text"><i class="fa-solid fa-star"></i></span>
+                            <input type="text" wire:model="form.registratrion_number" placeholder="registration number" class="form-control">
+                            @error('form.registratrion_number')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <label for="form.chassis_number">Chassis Number<span class="text-danger">*</span></label>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text"><i class="fa-solid fa-star"></i></span>
+                            <input type="text" wire:model="form.chassis_number" placeholder="chassis number" class="form-control">
+                            @error('form.chassis_number')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <label for="form.status">Status<span class="text-danger">*</span></label>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text"><i class="fa-solid fa-star"></i></span>
+                            <select wire:model="form.status" class="form-control @error('form.status') {{ 'is-invalid' }} @enderror">
                                 <option selected value>-- Please Select Status --</option>
                                 <option value="1">Active</option>
                                 <option value="0">Inactive</option>
@@ -73,7 +131,17 @@
         <!-- Header Part End !-->
 
         <!-- Details Part Start !-->
-        
+        <div class="row">
+            <div class="col-lg-5">
+                <p class="fs-6 fw-400">Vehicle Name</p>
+            </div>
+            <div class="col-lg-2">
+                <p class="fs-6 fw-400">:</p>
+            </div>
+            <div class="col-lg-5">
+                 <p class="fs-6 fw-400">Toyota CHR</p>
+            </div>
+        </div>
         <!-- Details Part End !-->
     </div>
 </div>
