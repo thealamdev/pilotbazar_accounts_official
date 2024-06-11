27726e674aeddbed37af3d40250bc9fedfcdff53
thealamdev <shahalam141072@gmail.com>
2024-06-11 15:52:05 +0600

    Form has been created for vehicle costing

--- app/Livewire/VehicleManagement/Stack/Vehicle/VehicleCosting/VehicleCostingComponent.php
+++ app/Livewire/VehicleManagement/Stack/Vehicle/VehicleCosting/VehicleCostingComponent.php
@@ -4,14 +4,44 @@ namespace App\Livewire\VehicleManagement\Stack\Vehicle\VehicleCosting;
 
 use Livewire\Component;
 use App\Models\VehicleManagement\Vehicle\Vehicle;
+use Livewire\Attributes\Title;
 
 class VehicleCostingComponent extends Component
 {
+    /**
+     * Define public property $mediaCostingForm;
+     * @var bool
+     */
+    public $mediaCostingForm = 'no';
+
+    /**
+     * Define public property $mediaCostFormLen
+     */
+    public $mediaCostFormLen = 1;
+
     /**
      * Define public property $vehicle
      * @var array|object
      */
     public ?object $vehicle;
+
+    /**
+     * Define public method updatedmediaCostingForm() for update $mediaCostingForm
+     * @return void
+     */
+    public function mediaCostingFormStatus()
+    {
+        $this->mediaCostingForm = $this->mediaCostingForm == 'no'  ? 'yes' : 'no';
+    }
+
+    /**
+     * Define public method mediaCostFormLenIncrement() for increment the mediaCostingForm
+     * @return void
+     */
+    public function mediaCostFormLenIncrement()
+    {
+        $this->mediaCostFormLen++;
+    }
 
     /**
      * Define the mount method
@@ -22,8 +52,18 @@ class VehicleCostingComponent extends Component
         $this->vehicle = $vehicle;
     }
 
+    /**
+     * Define public method saveMediaCost() for save the record
+     * @return void
+     */
+    public function saveMediaCost()
+    {
+        dd('Hi');
+    }
+
+    #[Title('Vehicle Costings')]
     public function render()
     {
-        return view('livewire.vehicle-management.stack.vehicle.vehicle-costing.vehicle-costing-component');
+        return view('livewire.vehicle-management.stack.vehicle.vehicle-costing.vehicle-costing-component', ['mediaCostingForm' => $this->mediaCostingForm, 'mediaCostFormLen' => $this->mediaCostFormLen]);
     }
 }
--- resources/views/livewire/vehicle-management/stack/vehicle/vehicle-costing/vehicle-costing-component.blade.php
+++ resources/views/livewire/vehicle-management/stack/vehicle/vehicle-costing/vehicle-costing-component.blade.php
@@ -2,91 +2,72 @@
     <!-- Header Part Start !-->
     <header class="pb-4 d-flex justify-content-between align-items-center">
         <div class="">
-            <p class="fs-5 fw-600">Vehicle Buy Payment of <span class="text-success">{{ $vehicle->name . ' ' . $vehicle->models?->name . ' ' . $vehicle->model_year?->name }}</span> </p>
+            <p class="fs-5 fw-600">Costings of <span class="text-success">{{ $vehicle->name . ' ' . $vehicle->models?->name . ' ' . $vehicle->model_year?->name }}</span> </p>
             <span>Please must fill the field where (<span class="text-danger">*</span>) sign is visible.</span>
         </div>
     </header>
     <!-- Header Part End !-->
 
     <!-- Form Part End !-->
-    <div class="border border-slate-400 rounded p-3">
-        <form action="#" wire:submit="saveBank" method="POST">
-            @csrf
-            <div class="row">
-                <div class="col-lg-6">
-                    <label for="form.pbl_bank">PBL Bank<span class="text-danger">*</span></label>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text">৳</span>
-                        <input type="text" wire:model.live="form.pbl_bank" class="form-control @error('form.pbl_bank') {{ 'is-invalid' }} @enderror" placeholder="client name">
-                        @error('form.pbl_bank')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <label for="form.client_name">Client Name<span class="text-danger">*</span></label>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text">৳</span>
-                        <input type="text" wire:model.live="form.client_name" class="form-control @error('form.client_name') {{ 'is-invalid' }} @enderror" placeholder="client name">
-                        @error('form.client_name')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <label for="form.clients_bank">Client Bank<span class="text-danger">*</span></label>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text">৳</span>
-                        <input type="text" wire:model.live="form.clients_bank" class="form-control @error('form.clients_bank') {{ 'is-invalid' }} @enderror" placeholder="client bank">
-                        @error('form.clients_bank')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <label for="form.client_bank_account_number">Account Number<span class="text-danger">*</span></label>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text">৳</span>
-                        <input type="text" wire:model.live="form.client_bank_account_number" class="form-control @error('form.client_bank_account_number') {{ 'is-invalid' }} @enderror" placeholder="client bacnk account number">
-                        @error('form.client_bank_account_number')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <label for="form.amount">Amount<span class="text-danger">*</span></label>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text">৳</span>
-                        <input type="number" wire:model.live="form.amount" class="form-control @error('form.amount') {{ 'is-invalid' }} @enderror" placeholder="amout">
-                        @error('form.amount')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <label for="form.date">Date<span class="text-danger">*</span></label>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text">৳</span>
-                        <input type="date" wire:model.live="form.date" class="form-control @error('form.date') {{ 'is-invalid' }} @enderror">
-                        @error('form.date')
-                            <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
-                    </div>
-                </div>
-                <div class="col-lg-12">
-                    <label for="form.remarks">Remark<span class="text-danger"></span></label>
-                    <div class="input-group mb-3">
-                        <textarea wire:model.live="form.remarks" cols="30" rows="5" class="form-control" placeholder="remakrs"></textarea>
-                    </div>
-                </div>
-            </div>
-
-            <div class="text-start">
-                <button class="bg-transparent border border-slate-400 px-4 py-1 rounded" type="submit">Save</button>
-                <button wire:click='skip' class="bg-transparent border border-blue-400 px-4 py-1 rounded" type="button">Skip &Next</button>
-            </div>
-
+    <div class="d-flex align-items-center" style="width: 200px">
+        <p for="mediaCosting">Media Costing</p>
+        <p wire:click='mediaCostingFormStatus' class="ms-5 border border-slate-400 text-center fs-4" style="border-radius: 50%;width:20px;height:20px;line-height:15px;cursor: pointer;">
+            {{ $mediaCostingForm == 'yes' ? '-' : '+' }}
+        </p>
     </div>
+    @if ($mediaCostingForm == 'yes')
+        <div class="border border-slate-400 rounded p-3 mt-5">
+            <form action="#" wire:submit="saveMediaCost" method="POST">
+                @csrf
+                @for ($i = 1; $i <= $mediaCostFormLen; $i++)
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <label for="form.costing_field">Client Name<span class="text-danger">*</span></label>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text">৳</span>
+                                <input type="text" wire:model.live="form.client_name" class="form-control @error('form.client_name') {{ 'is-invalid' }} @enderror" placeholder="client name">
+                                @error('form.client_name')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <label for="form.amount">Amount<span class="text-danger">*</span></label>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text">৳</span>
+                                <input type="number" wire:model.live="form.amount" class="form-control @error('form.amount') {{ 'is-invalid' }} @enderror" placeholder="amout">
+                                @error('form.amount')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <label for="form.date">Date<span class="text-danger">*</span></label>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text">৳</span>
+                                <input type="date" wire:model.live="form.date" class="form-control @error('form.date') {{ 'is-invalid' }} @enderror">
+                                @error('form.date')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <label for="form.remarks">Remark<span class="text-danger"></span></label>
+                            <div class="input-group mb-3">
+                                <textarea wire:model.live="form.remarks" cols="30" rows="3" class="form-control" placeholder="remakrs"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                @endfor
+
+                <div class="text-start">
+                    <button class="bg-transparent border border-slate-400 px-4 py-1 rounded" type="submit">Save</button>
+                    <button wire:click="mediaCostFormLenIncrement" class="bg-transparent border border-slate-400 px-4 py-1 rounded" type="button">+</button>
+                </div>
+            </form>
+        </div>
+    @endif
+
 </div>
 <!-- Form Part End !-->
 </div>
