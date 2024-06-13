8471721eda41357de1acd533547fe6b7227a650c
thealamdev <shahalam141072@gmail.com>
2024-06-13 15:56:21 +0600

    Side bar issue has been solved

--- resources/views/livewire/hooks/sidebar.blade.php
+++ resources/views/livewire/hooks/sidebar.blade.php
@@ -251,7 +251,7 @@
                      </ul>
                      <!-- 'Vehicle Management -> Dependency !-->
                      <ul class="navbar-nav flex-column">
-                         <li class="nav-item  @if (Route::is('admin.version1.vehicle-management.vehicle.dependency.payment.method.index') || Route::is('admin.version1.vehicle-management.vehicle.dependency.payment.method.create') || Route::is('admin.version1.vehicle-management.vehicle.dependency.bank.pbl_bank.create')) active @endif">
+                         <li class="nav-item @if (Route::is('admin.version1.vehicle-management.vehicle.dependency.payment.method.index') || Route::is('admin.version1.vehicle-management.vehicle.dependency.payment.method.create') || Route::is('admin.version1.vehicle-management.vehicle.dependency.bank.pbl_bank.create')) active @endif">
                              <a class="nav-link" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#dependency_pages">
                                  <span class="nav-icon-wrap">
                                      <span class="svg-icon">
@@ -265,32 +265,25 @@
                                  </span>
                                  <span class="nav-link-text">Dependency</span>
                              </a>
-                             <ul id="dependency_pages" class="nav flex-column collapse nav-children @if (Route::is('admin.version1.vehicle-management.vehicle.dependency.payment.method.index') || Route::is('admin.version1.vehicle-management.vehicle.dependency.payment.method.create') || Route::is('admin.version1.vehicle-management.vehicle.dependency.bank.pbl_bank.create')) show @endif">
+                             <ul id="dependency_pages" class="nav flex-column collapse  nav-children @if (Route::is('admin.version1.vehicle-management.vehicle.dependency.payment.method.index') || Route::is('admin.version1.vehicle-management.vehicle.dependency.payment.method.create') || Route::is('admin.version1.vehicle-management.vehicle.dependency.bank.pbl_bank.create')) show @endif">
                                  <li class="nav-item {{ Route::is('admin.version1.vehicle-management.vehicle.dependency.bank.pbl_bank.create') ? 'active' : '' }}">
-                                     <a class="nav-link" href="{{ route('admin.version1.vehicle-management.vehicle.dependency.bank.pbl_bank.create') }}">
-                                         <span class="nav-link-text">Bank</span>
-                                     </a>
-                                 </li>
-
-                                 <li class="nav-item">
                                      <ul class="nav flex-column">
                                          <li class="nav-item">
-                                             <a class="nav-link" href="javascript:;" data-bs-toggle="collapse" data-bs-target="#method_log">
+                                             <a class="nav-link" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#dash_log">
                                                  <span class="nav-link-text">Payment</span>
                                              </a>
-                                             <ul id="method_log" class="nav flex-column collapse  nav-children @if (Route::is('admin.version1.vehicle-management.vehicle.dependency.payment.method.index') || Route::is('admin.version1.vehicle-management.vehicle.dependency.payment.method.create')) show @endif">
-                                                 <li class="nav-item {{ Route::is('admin.version1.vehicle-management.vehicle.dependency.payment.method.index') || Route::is('admin.version1.vehicle-management.vehicle.dependency.payment.method.create') ? 'active' : '' }}">
+                                             <ul id="dash_log" class="nav flex-column collapse  nav-children {{ Route::is('admin.version1.vehicle-management.vehicle.dependency.payment.method.index') || Route::is('admin.version1.vehicle-management.vehicle.dependency.payment.method.create') ? 'show' : '' }}">
+                                                 <li class="nav-item">
                                                      <ul class="nav flex-column">
                                                          <li class="nav-item {{ Route::is('admin.version1.vehicle-management.vehicle.dependency.payment.method.index') || Route::is('admin.version1.vehicle-management.vehicle.dependency.payment.method.create') ? 'active' : '' }}">
-                                                             <a class="nav-link" href="{{ route('admin.version1.vehicle-management.vehicle.dependency.payment.method.index') }}">
-                                                                 <span class="nav-link-text">
-                                                                     Method
-                                                                 </span>
-                                                             </a>
+                                                             <a class="nav-link" href="{{ route('admin.version1.vehicle-management.vehicle.dependency.payment.method.create') }}"><span class="nav-link-text">Method</span></a>
                                                          </li>
                                                      </ul>
                                                  </li>
                                              </ul>
+                                         </li>
+                                         <li class="nav-item {{ Route::is('admin.version1.vehicle-management.vehicle.dependency.bank.pbl_bank.create') ? 'active' : '' }}">
+                                             <a class="nav-link" href="{{ route('admin.version1.vehicle-management.vehicle.dependency.bank.pbl_bank.create') }}"><span class="nav-link-text">Bank</span></a>
                                          </li>
                                      </ul>
                                  </li>
