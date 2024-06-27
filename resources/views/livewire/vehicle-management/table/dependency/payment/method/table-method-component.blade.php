1e28af099be8ded99c1fd85d41e5430a5609c793
thealamdev <shahalam141072@gmail.com>
2024-06-27 18:52:51 +0600

    Delete alert has been added

--- resources/views/livewire/vehicle-management/table/dependency/payment/method/table-method-component.blade.php
+++ resources/views/livewire/vehicle-management/table/dependency/payment/method/table-method-component.blade.php
@@ -98,7 +98,7 @@
                                                                         </span>
                                                                         <span>Edit Method</span>
                                                                     </a>
-                                                                    <button class="dropdown-item" wire:click="delete({{ $each->id }})">
+                                                                    <button class="dropdown-item" wire:click="delete({{ $each->id }})" wire:confirm="Are you sure you want to delete this post?">
                                                                         <span class="feather-icon dropdown-icon">
                                                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2">
                                                                                 <polyline points="3 6 5 6 21 6"></polyline>
