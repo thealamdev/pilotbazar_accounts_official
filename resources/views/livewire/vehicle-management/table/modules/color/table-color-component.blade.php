a0f0abc8f060731105b35b8b75df49755a008b3a
thealamdev <shahalam141072@gmail.com>
2024-06-28 15:21:43 +0600

    Modules edit and delete has been done

--- resources/views/livewire/vehicle-management/table/modules/color/table-color-component.blade.php
+++ resources/views/livewire/vehicle-management/table/modules/color/table-color-component.blade.php
@@ -90,26 +90,8 @@
                                                                     </span>
                                                                 </button>
                                                                 <div class="dropdown-menu dropdown-menu-end">
-                                                                    <a class="dropdown-item" href="{{ route('admin.version1.vehicle-management.vehicle.module.color.update', ['color' => $each->id]) }}">
-                                                                        <span class="feather-icon dropdown-icon">
-                                                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit">
-                                                                                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-                                                                                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-                                                                            </svg>
-                                                                        </span>
-                                                                        <span>Edit Method</span>
-                                                                    </a>
-                                                                    <button class="dropdown-item" wire:click="delete({{ $each->id }})" wire:confirm="Are you sure you want to delete this post?">
-                                                                        <span class="feather-icon dropdown-icon">
-                                                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2">
-                                                                                <polyline points="3 6 5 6 21 6"></polyline>
-                                                                                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-                                                                                <line x1="10" y1="11" x2="10" y2="17"></line>
-                                                                                <line x1="14" y1="11" x2="14" y2="17"></line>
-                                                                            </svg>
-                                                                        </span>
-                                                                        <span>Delete</span>
-                                                                    </button>
+                                                                    <x-action.edit name="Edit Vehicle" route="{{ route('admin.version1.vehicle-management.vehicle.module.color.update', ['color' => $each->id]) }}" />
+                                                                    <x-action.delete name="Delete" :id="$each->id" />
                                                                 </div>
                                                             </div>
                                                         </td>
