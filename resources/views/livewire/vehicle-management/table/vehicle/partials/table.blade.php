a7bef8f16b5823bc7645c358f3af3e66856f0037
thealamdev <shahalam141072@gmail.com>
2024-06-28 15:26:28 +0600

    Modules edit and delete has been done

--- resources/views/livewire/vehicle-management/table/vehicle/partials/table.blade.php
+++ resources/views/livewire/vehicle-management/table/vehicle/partials/table.blade.php
@@ -43,17 +43,7 @@
                 <td class="text-end">{!! Helper::ISOdate($each?->updated_at) !!}</td>
                 <td class="text-end">
                     <div class="dropdown">
-                        <button class="btn btn-icon btn-flush-dark btn-rounded flush-soft-hover dropdown-toggle no-caret" aria-expanded="false" data-bs-toggle="dropdown">
-                            <span class="icon">
-                                <span class="feather-icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical">
-                                        <circle cx="12" cy="12" r="1"></circle>
-                                        <circle cx="12" cy="5" r="1"></circle>
-                                        <circle cx="12" cy="19" r="1"></circle>
-                                    </svg>
-                                </span>
-                            </span>
-                        </button>
+                        <x-action.action-button />
                         <div class="dropdown-menu dropdown-menu-end" style="">
                             <x-action.edit name="Edit Vehicle" route="{{ route('admin.version1.vehicle-management.vehicle.module.color.update', ['color' => $each->id]) }}" />
                             <x-action.delete name="Delete" :id="$each->id" />
