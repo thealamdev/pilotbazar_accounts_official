9056d54228e3aa4465c7ca12545a904096b33065
thealamdev <shahalam141072@gmail.com>
2024-08-12 11:02:20 +0600

    Some modification has been commited

--- resources/views/livewire/vehicle-management/stack/vehicle/vehicle-buy-payment/create-vehicle-buy-payment-component.blade.php
+++ resources/views/livewire/vehicle-management/stack/vehicle/vehicle-buy-payment/create-vehicle-buy-payment-component.blade.php
@@ -2,7 +2,7 @@
     <!-- Header Part Start !-->
     <header class="pb-4 d-flex justify-content-between align-items-center">
         <div class="">
-            <p class="fs-5 fw-600">Vehicle Buy Payment of <span class="text-success">{{ $vehicle->name . ' ' . $vehicle->models?->name . ' ' . $vehicle->model_year?->name }}</span> </p>
+            <p class="fs-5 fw-600">Vehicle Buy Payment of <a href="{{ route('admin.version1.vehicle-management.vehicle.show',[p]) }}"><span class="text-success">{{ $vehicle->name . ' ' . $vehicle->models?->name . ' ' . $vehicle->model_year?->name }}</span></a> </p>
             <span>Please must fill the field where (<span class="text-danger">*</span>) sign is visible.</span>
         </div>
     </header>
