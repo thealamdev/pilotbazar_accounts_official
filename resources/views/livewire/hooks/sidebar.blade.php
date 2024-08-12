b4e9c1cbb9f38bfcc6a15f4b99453393ae6133aa
thealamdev <shahalam141072@gmail.com>
2024-08-12 16:38:41 +0600

    Investor icon has been added

--- resources/views/livewire/hooks/sidebar.blade.php
+++ resources/views/livewire/hooks/sidebar.blade.php
@@ -305,13 +305,12 @@
                                  <a class="nav-link" href="{{ route('admin.version1.vehicle-management.vehicle.index') }}">
                                      <span class="nav-icon-wrap">
                                          <span class="svg-icon">
-                                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-template" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                 <rect x="4" y="4" width="16" height="4" rx="1" />
-                                                 <rect x="4" y="12" width="6" height="8" rx="1" />
-                                                 <line x1="14" y1="12" x2="20" y2="12" />
-                                                 <line x1="14" y1="16" x2="20" y2="16" />
-                                                 <line x1="14" y1="20" x2="20" y2="20" />
+                                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                 <circle cx="12" cy="7" r="4"></circle>
+                                                 <path d="M6 21v-2a4 4 0 0 1 4 -4h1"></path>
+                                                 <circle cx="16.5" cy="17.5" r="2.5"></circle>
+                                                 <path d="M18.5 19.5l2.5 2.5"></path>
                                              </svg>
                                          </span>
                                      </span>
