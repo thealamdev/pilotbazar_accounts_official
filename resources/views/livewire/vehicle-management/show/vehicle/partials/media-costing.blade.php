b9ca981d568c0db9aec53acb8de70ee1fa71baa2
thealamdev <shahalam141072@gmail.com>
2024-08-17 15:10:01 +0600

    Problem has been solved

--- resources/views/livewire/vehicle-management/show/vehicle/partials/media-costing.blade.php
+++ resources/views/livewire/vehicle-management/show/vehicle/partials/media-costing.blade.php
@@ -14,16 +14,26 @@
  <div class="d-flex justify-content-between pb-3">
      <p style="width:30%"><span class="border-bottom">Costing Name</span></p>
      <p style="width:25%"><span class="border-bottom">Date</span></p>
-     <p style="width:30%"><span class="border-bottom">Remarks</span></p>
-     <p style="width:15%;text-align:end"><span class="border-bottom">Amount</span></p>
+     <p style="width:25%"><span class="border-bottom">Remarks</span></p>
+     <p style="width:10%;text-align:end"><span class="border-bottom">Amount</span></p>
+     <p style="width:10%; text-align:end"><span class="border-bottom">Edit</span></p>
  </div>
 
  @forelse ($response?->mediaCosting as $each)
      <div class="d-flex justify-content-between pb-1">
          <p style="width:30%">{{ $each?->costing_name }}</p>
          <p style="width:25%">{{ date('d M, Y', strtotime($each?->date)) }}</p>
-         <p style="width:30%" title="{{ $each?->remarks }}">{{ Str::limit($each?->remarks, 25, '...') }}</p>
-         <p style="width:15%;text-align:end">{{ $each?->amount }} tk</p>
+         <p style="width:25%" title="{{ $each?->remarks }}">{{ Str::limit($each?->remarks, 25, '...') }}</p>
+         <p style="width:10%;text-align:end">{{ $each?->amount }} tk</p>
+         <p style="width:10%;text-align:end">
+             <a style="margin-top: -10px;" class="btn btn-icon btn-flush-dark btn-rounded flush-soft-hover" href="{{ route('admin.version1.vehicle-management.vehicle.create') }}">
+                 <span class="icon">
+                     <span class="feather-icon">
+                         <i class="fa-solid fa-pen" style="color:gray"></i>
+                     </span>
+                 </span>
+             </a>
+         </p>
      </div>
  @empty
      <div class="text-center">
