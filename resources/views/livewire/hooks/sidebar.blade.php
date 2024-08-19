aa34e009080213023ae491e49124da09b27bdc9a
thealamdev <shahalam141072@gmail.com>
2024-08-19 12:41:28 +0600

    Expense has been added into the sidebar

--- resources/views/livewire/hooks/sidebar.blade.php
+++ resources/views/livewire/hooks/sidebar.blade.php
@@ -420,6 +420,145 @@
                  </div>
                  <!-- Investor menu end !-->
 
+                 <!-- Expenses menu start !-->
+                 <div class="menu-gap"></div>
+                 <div class="menu-group">
+                     <div class="nav-header">
+                         <span>Expense Management</span>
+                     </div>
+                     <div class="menu-group">
+                         <ul class="navbar-nav flex-column">
+                             <li class="nav-item {{ Route::is('admin.version1.investor-management.investor.index') ? 'active' : '' }}">
+                                 <a class="nav-link" href="{{ route('admin.version1.investor-management.investor.index') }}">
+                                     <span class="nav-icon-wrap">
+                                         <span class="svg-icon">
+                                             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                 <circle cx="12" cy="7" r="4"></circle>
+                                                 <path d="M6 21v-2a4 4 0 0 1 4 -4h1"></path>
+                                                 <circle cx="16.5" cy="17.5" r="2.5"></circle>
+                                                 <path d="M18.5 19.5l2.5 2.5"></path>
+                                             </svg>
+                                         </span>
+                                     </span>
+                                     <span class="nav-link-text">Daily Expense</span>
+                                 </a>
+                             </li>
+                             <li class="nav-item {{ Route::is('admin.version1.investor-management.investor.investment.index') ? 'active' : '' }}">
+                                <a class="nav-link" href="{{ route('admin.version1.investor-management.investor.investment.index') }}">
+                                    <span class="nav-icon-wrap">
+                                        <span class="svg-icon">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                <circle cx="12" cy="7" r="4"></circle>
+                                                <path d="M6 21v-2a4 4 0 0 1 4 -4h1"></path>
+                                                <circle cx="16.5" cy="17.5" r="2.5"></circle>
+                                                <path d="M18.5 19.5l2.5 2.5"></path>
+                                            </svg>
+                                        </span>
+                                    </span>
+                                    <span class="nav-link-text">Expense Category</span>
+                                </a>
+                            </li>
+                            <li class="nav-item {{ Route::is('admin.version1.investor-management.investor.investment.index') ? 'active' : '' }}">
+                                <a class="nav-link" href="{{ route('admin.version1.investor-management.investor.investment.index') }}">
+                                    <span class="nav-icon-wrap">
+                                        <span class="svg-icon">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                                <circle cx="12" cy="7" r="4"></circle>
+                                                <path d="M6 21v-2a4 4 0 0 1 4 -4h1"></path>
+                                                <circle cx="16.5" cy="17.5" r="2.5"></circle>
+                                                <path d="M18.5 19.5l2.5 2.5"></path>
+                                            </svg>
+                                        </span>
+                                    </span>
+                                    <span class="nav-link-text">Expense Sub Category</span>
+                                </a>
+                            </li>
+                         </ul>
+                     </div>
+                     {{-- <!-- Vehicle Management -> Modules !-->
+                     <ul class="navbar-nav flex-column">
+                         <li class="nav-item  @if (Route::is('admin.version1.vehicle-management.vehicle.module.*')) active @endif">
+                             <a class="nav-link" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#dash_pages">
+                                 <span class="nav-icon-wrap">
+                                     <span class="svg-icon">
+                                         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                             <circle cx="9" cy="7" r="4" />
+                                             <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                                             <path d="M16 11h6m-3 -3v6" />
+                                         </svg>
+                                     </span>
+                                 </span>
+                                 <span class="nav-link-text">Modules</span>
+                             </a>
+                             <ul id="dash_pages" class="nav flex-column collapse nav-children @if (Route::is('admin.version1.vehicle-management.vehicle.module.*')) show @endif">
+                                 <li class="nav-item">
+                                     <ul class="nav flex-column">
+                                         <li class="nav-item {{ Route::is('admin.version1.vehicle-management.vehicle.module.color.*') ? 'active' : '' }}">
+                                             <a class="nav-link" href="{{ route('admin.version1.vehicle-management.vehicle.module.color.index') }}">
+                                                 <span class="nav-link-text">Color</span>
+                                             </a>
+                                         </li>
+                                         <li class="nav-item {{ Route::is('admin.version1.vehicle-management.vehicle.module.model.*') ? 'active' : '' }}">
+                                             <a class="nav-link" href="{{ route('admin.version1.vehicle-management.vehicle.module.model.index') }}">
+                                                 <span class="nav-link-text">Model</span>
+                                             </a>
+                                         </li>
+                                         <li class="nav-item {{ Route::is('admin.version1.vehicle-management.vehicle.module.model-year.*') ? 'active' : '' }}">
+                                             <a class="nav-link" href="{{ route('admin.version1.vehicle-management.vehicle.module.model-year.index') }}"><span class="nav-link-text">Model Year</span></a>
+                                         </li>
+                                     </ul>
+                                 </li>
+                             </ul>
+                         </li>
+                     </ul>
+                     <!-- Vehicle Management -> Dependency !-->
+                     <ul class="navbar-nav flex-column">
+                         <li class="nav-item @if (Route::is('admin.version1.vehicle-management.vehicle.dependency.*')) active @endif">
+                             <a class="nav-link" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#dependency_pages">
+                                 <span class="nav-icon-wrap">
+                                     <span class="svg-icon">
+                                         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                             <circle cx="9" cy="7" r="4" />
+                                             <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                                             <path d="M16 11h6m-3 -3v6" />
+                                         </svg>
+                                     </span>
+                                 </span>
+                                 <span class="nav-link-text">Dependency</span>
+                             </a>
+                             <ul id="dependency_pages" class="nav flex-column collapse  nav-children @if (Route::is('admin.version1.vehicle-management.vehicle.dependency.*')) show @endif">
+                                 <li class="nav-item {{ Route::is('admin.version1.vehicle-management.vehicle.dependency.bank.pbl_bank.*') ? 'active' : '' }}">
+                                     <ul class="nav flex-column">
+                                         <li class="nav-item">
+                                             <a class="nav-link" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#dash_log">
+                                                 <span class="nav-link-text">Payment</span>
+                                             </a>
+                                             <ul id="dash_log" class="nav flex-column collapse  nav-children {{ Route::is('admin.version1.vehicle-management.vehicle.dependency.payment.method.*') ? 'show' : '' }}">
+                                                 <li class="nav-item">
+                                                     <ul class="nav flex-column">
+                                                         <li class="nav-item {{ Route::is('admin.version1.vehicle-management.vehicle.dependency.payment.method.*') ? 'active' : '' }}">
+                                                             <a class="nav-link" href="{{ route('admin.version1.vehicle-management.vehicle.dependency.payment.method.index') }}"><span class="nav-link-text">Method</span></a>
+                                                         </li>
+                                                     </ul>
+                                                 </li>
+                                             </ul>
+                                         </li>
+                                         <li class="nav-item {{ Route::is('admin.version1.vehicle-management.vehicle.dependency.bank.pbl_bank.*') ? 'active' : '' }}">
+                                             <a class="nav-link" href="{{ route('admin.version1.vehicle-management.vehicle.dependency.bank.pbl_bank.index') }}"><span class="nav-link-text">Bank</span></a>
+                                         </li>
+                                     </ul>
+                                 </li>
+                             </ul>
+                         </li>
+                     </ul> --}}
+                 </div>
+                 <!-- Expenses menu end !-->
+
                  <div class="menu-gap"></div>
                  <div class="menu-group">
                      <div class="nav-header">
