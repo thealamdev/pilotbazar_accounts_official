46baf1803083872ed4c0cf80131a8a3c1f37a744
thealamdev <shahalam141072@gmail.com>
2024-08-19 16:33:19 +0600

    Route has been create in sub category

---
+++ app/Livewire/ExpenseManagement/Stack/ExpenseSubCategory/CreateExpenseSubCategoryComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\ExpenseManagement\Stack\ExpenseSubCategory;
+
+use Livewire\Component;
+
+class CreateExpenseSubCategoryComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.expense-management.stack.expense-sub-category.create-expense-sub-category-component');
+    }
+}
--- routes/version1/expense-management/base.php
+++ routes/version1/expense-management/base.php
@@ -12,4 +12,10 @@ Route::prefix('expenses')->name('expense.')->group(function () {
         Route::get('create', \App\Livewire\ExpenseManagement\Stack\ExpenseCategory\CreateExpenseCategoryComponent::class)->name('create');
         Route::get('update/{expenseCategory}', \App\Livewire\ExpenseManagement\Stack\ExpenseCategory\UpdateExpenseCategoryComponent::class)->name('update');
     });
+
+    Route::prefix('sub-categories')->name('sub-category.')->group(function () {
+        // Route::get('/', \App\Livewire\ExpenseManagement\Table\ExpenseSubCategory\TableExpenseCategoryComponent::class)->name('index');
+        Route::get('create', \App\Livewire\ExpenseManagement\Stack\ExpenseSubCategory\CreateExpenseSubCategoryComponent::class)->name('create');
+        // Route::get('update/{expenseCategory}', \App\Livewire\ExpenseManagement\Stack\ExpenseCategory\UpdateExpenseCategoryComponent::class)->name('update');
+    });
 });
